c33050c470edf44ea300bfc87c1d974c2b80fccc
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-11 20:51:30 +0000

    Added additional function that will download pictures, however the functions are not linked yet.

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var fs = require("fs");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -23,10 +24,41 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result: ");
-  result.forEach(function(avatar) {
-    console.log("\t", avatar.avatar_url);
-  });
-});
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result: ");
+//   result.forEach(function(avatar) {
+//     console.log("\t", avatar.avatar_url);
+//   });
+// });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on("error", function(err) {
+      console.log("Error: ", err);
+      return;
+    })
+    .on("response", function(resp) {
+      console.log(`Response code is ${resp.statusCode} and response message is:`, resp.statusMessage);
+      console.log("Beginning download..");
+    })
+    .pipe(fs.createWriteStream(filePath))
+    .on("finish", function() {
+      console.log("Download complete.");
+    });
+}
+
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./newimage.jpg");
+
+
+
+
+
+
+
+
+
+
+
+
